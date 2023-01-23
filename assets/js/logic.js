a2c7e3ffe24468b4acd9e448a95992f6085953cc
Pete Andrew <112977421+Pete-Andrew@users.noreply.github.com>
2023-01-23 13:20:16 +0000

    working colour schemes for hours

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,25 +1,42 @@
-var weekDay = moment();
-$("#currentDay").text(weekDay.format("dddd, Do, MMMM"));
-
+// get current time from moment
 var time = moment();
 
-// get current time from moment
-// Assign current time to a var, use jquery to select the id 'current time' give it text (e.g. the time)
-function currentTime () {
-var currentTime = $("#currentTime").text(time.format("hh:mm:ss"));
-// $("#currentTime").hide();
-console.log(currentTime);
+// places the date in the jumbotron
+$("#currentDay").text(time.format("dddd, Do, MMMM"));
+
+
+//creates a var 'timeNow' and converts the moment time hour value into an integer usuing parseInt
+var timeNow = parseInt(time.format("H"));
+console.log("The time is after: " + timeNow);
+
+//turns the timeBlocks in HTML into an array so that each timeBlock can be accessed independently  
+// or cycled through as an array. the '.desciption' class specifiaclly targets the textarea.   
+var timeBlockArray = Array.from($(".description"));
+    // console.log(timeBlockArray);
+
+// cycles through the timeBlocks array 
+for (var i = 0; i < timeBlockArray.length; i++) {  
+    
+    //uses parseInt for each timeBlock id (e.g. changes the id value (9-17) to an intiger using parseInt).    
+    var timeBlockId = parseInt(timeBlockArray[i].id);
+    console.log(timeBlockId);
+    
+    // the if statement checks the timeBlockArray agains 'time now' and changes the colours accordingly to the css presets
+    //classList.add 
+    if (timeBlockId === timeNow) {
+        timeBlockArray[i].classList.add("present");
+    }   
+    else if (timeBlockId < timeNow) {
+        timeBlockArray[i].classList.add("past");
+    }
+    else if (timeBlockId > timeNow) {
+        timeBlockArray[i].classList.add("future");
+    }
+
+
 }
 
-currentTime();
 
-function currentHour() { 
-var currentHour = $("#currentHour").text(time.format("H")); 
-console.log(currentHour);
-// $("#currentHour").hide();
-}
-
-currentHour(); 
 
 // The app should:
 
@@ -45,3 +62,22 @@ currentHour();
 
 // ![A user clicks on slots on the color-coded calendar and edits the events.](./images/05-third-party-apis-homework-demo.gif)
 
+
+// spare code:
+
+
+//// Assign current time to a var, use jquery to select the id 'current time' give it text (e.g. the time)
+// function currentTime () {
+//     var currentTime = $("#currentTime").text(time.format("hh:mm:ss"));
+//     $("#currentTime").hide();  
+//     }
+// currentTime();
+
+////takes the current time variable and turns it into 24h hour only time
+// function currentHourValue() { 
+//     var currentHour = $("#currentHour").text(time.format("H")); 
+//     console.log(currentHour);
+//     $("#currentHour").hide();
+//     // return currentHour;
+//     }
+// currentHourValue(); 
